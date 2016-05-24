b50d1b4da4c319b021ea5b6738204ff7269069a8
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-05-24 11:53:10 -0300

    Integração JS e Sonar

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@
     var uglify = require("gulp-uglify");
     var minify = require('gulp-minify');
     var concat = require('gulp-concat');
+    var sonar = require('gulp-sonar');
+    var packageJson = require('./package.json');
 
     gulp.task('upgrade-version', function(value) {
         gulp.src('./package.json')
@@ -21,6 +23,36 @@
                 'mangle': false
             }))
             .pipe(gulp.dest('dist'));
+    });
+
+    gulp.task('sonar', function() {
+        var options = {
+            sonar: {
+                host: {
+                    url: process.env.npm_config_sonarUrl,
+                },
+                jdbc: {
+                    url: process.env.npm_config_sonarDatabaseUrl,
+                    username: process.env.npm_config_sonarDatabaseUsername,
+                    password: process.env.npm_config_sonarDatabasePassword
+                },
+                projectKey: 'sonar:otus-client-js',
+                projectName: 'otus-client-js',
+                projectVersion: packageJson.version,
+                // comma-delimited string of source directories
+                sources: 'app',
+                language: 'js',
+                sourceEncoding: 'UTF-8',
+                exec: {
+                    maxBuffer: 1024 * 1024
+                }
+            }
+        };
+
+        return gulp.src('thisFileDoesNotExist.js', {
+                read: false
+            })
+            .pipe(sonar(options));
     });
 
 }());
