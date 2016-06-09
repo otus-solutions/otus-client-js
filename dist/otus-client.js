ca3373e8c58a7f0813999f01d97f0eb7ec188599
jenkins <jenkins@ccem.ufrgs.br>
2016-06-09 10:52:18 -0300

    JENKINS - Update Versão

--- dist/otus-client.js
+++ dist/otus-client.js
@@ -63,39 +63,6 @@
 
     angular
         .module('otus.client')
-        .factory('OtusInstallerResourceFactory', OtusInstallerResourceFactory);
-
-    OtusInstallerResourceFactory.$inject = ['$resource'];
-
-    function OtusInstallerResourceFactory($resource) {
-        var SUFFIX = '/installer';
-
-        var self = this;
-        self.create = create;
-
-        function create(restPrefix) {
-            return $resource({}, {}, {
-                ready: {
-                    method: 'GET',
-                    url: restPrefix + SUFFIX + '/ready'
-                },
-                config: {
-                    method: 'POST',
-                    url: restPrefix + SUFFIX + '/config'
-                }
-            });
-        }
-
-        return self;
-    }
-
-}());
-
-(function() {
-    'use strict';
-
-    angular
-        .module('otus.client')
         .factory('OtusAuthenticatorResourceFactory', OtusAuthenticatorResourceFactory);
 
     OtusAuthenticatorResourceFactory.$inject = ['$resource', '$window'];
@@ -128,6 +95,39 @@
 
         return self;
 
+    }
+
+}());
+
+(function() {
+    'use strict';
+
+    angular
+        .module('otus.client')
+        .factory('OtusInstallerResourceFactory', OtusInstallerResourceFactory);
+
+    OtusInstallerResourceFactory.$inject = ['$resource'];
+
+    function OtusInstallerResourceFactory($resource) {
+        var SUFFIX = '/installer';
+
+        var self = this;
+        self.create = create;
+
+        function create(restPrefix) {
+            return $resource({}, {}, {
+                ready: {
+                    method: 'GET',
+                    url: restPrefix + SUFFIX + '/ready'
+                },
+                config: {
+                    method: 'POST',
+                    url: restPrefix + SUFFIX + '/config'
+                }
+            });
+        }
+
+        return self;
     }
 
 }());
