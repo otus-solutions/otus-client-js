76a52aa3c813292d1379d06a6c5370a319a1ae68
jenkins <jenkins@ccem.ufrgs.br>
2016-07-07 19:42:55 -0300

    JENKINS - Update Versão

--- dist/otus-client.js
+++ dist/otus-client.js
@@ -32,54 +32,6 @@
 
     angular
         .module('otus.client')
-        .factory('OtusFieldCenterResourceFactory', OtusFieldCenterResourceFactory);
-
-    OtusFieldCenterResourceFactory.$inject = ['$resource', 'OtusRestResourceContext'];
-
-    function OtusFieldCenterResourceFactory($resource, OtusRestResourceContext) {
-        var SUFFIX = '/center';
-
-        var self = this;
-        self.create = create;
-
-        function create() {
-            return $resource({}, {}, {
-                getAll: {
-                    method: 'GET',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX,
-                    headers: {
-                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityProjectToken()
-                    }
-                },
-                create: {
-                    method: 'POST',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX,
-                    headers: {
-                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityProjectToken()
-                    }
-                },
-                update: {
-                    method: 'POST',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/update',
-                    headers: {
-                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityProjectToken()
-                    }
-                }
-
-            });
-        }
-
-        return self;
-
-    }
-
-}());
-
-(function() {
-    'use strict';
-
-    angular
-        .module('otus.client')
         .service('OtusRestResourceContext', OtusRestResourceContext);
 
     OtusRestResourceContext.$inject = ['$window', 'UrlParser'];
@@ -238,6 +190,54 @@
                 }
             });
 
+        }
+
+        return self;
+
+    }
+
+}());
+
+(function() {
+    'use strict';
+
+    angular
+        .module('otus.client')
+        .factory('OtusFieldCenterResourceFactory', OtusFieldCenterResourceFactory);
+
+    OtusFieldCenterResourceFactory.$inject = ['$resource', 'OtusRestResourceContext'];
+
+    function OtusFieldCenterResourceFactory($resource, OtusRestResourceContext) {
+        var SUFFIX = '/center';
+
+        var self = this;
+        self.create = create;
+
+        function create() {
+            return $resource({}, {}, {
+                getAll: {
+                    method: 'GET',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX,
+                    headers: {
+                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityProjectToken()
+                    }
+                },
+                create: {
+                    method: 'POST',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX,
+                    headers: {
+                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityProjectToken()
+                    }
+                },
+                update: {
+                    method: 'POST',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/update',
+                    headers: {
+                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityProjectToken()
+                    }
+                }
+
+            });
         }
 
         return self;
