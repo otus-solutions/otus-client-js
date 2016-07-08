c8d85179609e0e5ee55e68f0a60d4ad36777e8ff
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-07-07 21:26:10 -0300

    CEN-9 Correção Set Token

--- app/api/rest-resource-service.js
+++ app/api/rest-resource-service.js
@@ -14,6 +14,7 @@
         self.getOtusFieldCenterResource = getOtusFieldCenterResource;
         self.setUrl = setUrl;
 	self.setSecurityProjectToken = setSecurityProjectToken;
+	self.setSecurityToken = setSecurityToken;
 
         function setUrl(url) {
             OtusRestResourceContext.setUrl(url);
