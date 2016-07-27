9e28b88fedb566fa6b7527a1c18fd238d81d46eb
Diego Santos <santos.diegoferreira@gmail.com>
2016-07-27 17:12:12 -0300

    Atualização dist que o emanoel não fez.

--- dist/otus-client.js
+++ dist/otus-client.js
@@ -200,54 +200,6 @@
 
     angular
         .module('otus.client')
-        .factory('OtusAuthenticatorResourceFactory', OtusAuthenticatorResourceFactory);
-
-    OtusAuthenticatorResourceFactory.$inject = ['$resource', 'OtusRestResourceContext'];
-
-    function OtusAuthenticatorResourceFactory($resource, OtusRestResourceContext) {
-        var SUFFIX = '/authentication';
-
-        var self = this;
-        self.create = create;
-
-        function create() {
-            return $resource({}, {}, {
-                authenticate: {
-                    method: 'POST',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX,
-                    headers: {
-                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityToken()
-                    }
-                },
-                invalidate: {
-                    method: 'POST',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/invalidate',
-                    headers: {
-                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityToken()
-                    }
-                },
-                authenticateProject: {
-                    method: 'POST',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/project',
-                    headers: {
-                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityProjectToken()
-                    }
-                }
-            });
-
-        }
-
-        return self;
-
-    }
-
-}());
-
-(function() {
-    'use strict';
-
-    angular
-        .module('otus.client')
         .factory('OtusFieldCenterResourceFactory', OtusFieldCenterResourceFactory);
 
     OtusFieldCenterResourceFactory.$inject = ['$resource', 'OtusRestResourceContext'];
@@ -283,6 +235,54 @@
                 }
 
             });
+        }
+
+        return self;
+
+    }
+
+}());
+
+(function() {
+    'use strict';
+
+    angular
+        .module('otus.client')
+        .factory('OtusAuthenticatorResourceFactory', OtusAuthenticatorResourceFactory);
+
+    OtusAuthenticatorResourceFactory.$inject = ['$resource', 'OtusRestResourceContext'];
+
+    function OtusAuthenticatorResourceFactory($resource, OtusRestResourceContext) {
+        var SUFFIX = '/authentication';
+
+        var self = this;
+        self.create = create;
+
+        function create() {
+            return $resource({}, {}, {
+                authenticate: {
+                    method: 'POST',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX,
+                    headers: {
+                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityToken()
+                    }
+                },
+                invalidate: {
+                    method: 'POST',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/invalidate',
+                    headers: {
+                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityToken()
+                    }
+                },
+                authenticateProject: {
+                    method: 'POST',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/project',
+                    headers: {
+                        'Authorization': 'Bearer ' + OtusRestResourceContext.getSecurityProjectToken()
+                    }
+                }
+            });
+
         }
 
         return self;
