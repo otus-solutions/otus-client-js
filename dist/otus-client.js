ede822ea2faf837f4925c33396a8b94082580fe2
jenkins <jenkins@ccem.ufrgs.br>
2016-08-04 12:37:39 -0300

    JENKINS - Update Versão

--- dist/otus-client.js
+++ dist/otus-client.js
@@ -275,44 +275,6 @@
 
     angular
         .module('otus.client')
-        .factory('OtusInstallerResourceFactory', OtusInstallerResourceFactory);
-
-    OtusInstallerResourceFactory.$inject = ['$resource', 'OtusRestResourceContext'];
-
-    function OtusInstallerResourceFactory($resource, OtusRestResourceContext) {
-        var SUFFIX = '/installer';
-
-        var self = this;
-        self.create = create;
-
-        function create() {
-            return $resource({}, {}, {
-                ready: {
-                    method: 'GET',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/ready'
-                },
-                config: {
-                    method: 'POST',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/config'
-                },
-                validation: {
-                    method: 'POST',
-                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/validation'
-                }
-
-            });
-        }
-
-        return self;
-    }
-
-}());
-
-(function() {
-    'use strict';
-
-    angular
-        .module('otus.client')
         .factory('OtusFieldCenterResourceFactory', OtusFieldCenterResourceFactory);
 
     OtusFieldCenterResourceFactory.$inject = ['$resource', 'OtusRestResourceContext'];
@@ -352,6 +314,44 @@
 
         return self;
 
+    }
+
+}());
+
+(function() {
+    'use strict';
+
+    angular
+        .module('otus.client')
+        .factory('OtusInstallerResourceFactory', OtusInstallerResourceFactory);
+
+    OtusInstallerResourceFactory.$inject = ['$resource', 'OtusRestResourceContext'];
+
+    function OtusInstallerResourceFactory($resource, OtusRestResourceContext) {
+        var SUFFIX = '/installer';
+
+        var self = this;
+        self.create = create;
+
+        function create() {
+            return $resource({}, {}, {
+                ready: {
+                    method: 'GET',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/ready'
+                },
+                config: {
+                    method: 'POST',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/config'
+                },
+                validation: {
+                    method: 'POST',
+                    url: OtusRestResourceContext.getRestPrefix() + SUFFIX + '/validation'
+                }
+
+            });
+        }
+
+        return self;
     }
 
 }());
