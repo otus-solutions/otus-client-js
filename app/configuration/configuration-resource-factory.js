4d50be14757692da8d09bac5aa82ede357219c04
brenoscheffer <brenoscheffer@gmail.com>
2016-10-05 19:33:56 -0300

    OTUS-20 Refatoração

--- app/configuration/configuration-resource-factory.js
+++ app/configuration/configuration-resource-factory.js
@@ -1,66 +1,76 @@
 (function() {
-    'use strict';
+   'use strict';
 
-    angular
-        .module('otus.client')
-        .factory('otusjs.otus.client.OtusProjectConfigurationResourceFactory', Factory);
+   angular
+      .module('otus.client')
+      .factory('otusjs.otus.client.OtusProjectConfigurationResourceFactory', Factory);
 
-    Factory.$inject = [
+   Factory.$inject = [
         '$resource',
         'OtusRestResourceContext',
         'otus.client.HeaderBuilderFactory'
     ];
 
-    function Factory($resource, OtusRestResourceContext, HeaderBuilderFactory) {
-        var SUFFIX = '/configuration';
+   function Factory($resource, OtusRestResourceContext, HeaderBuilderFactory) {
+      var SUFFIX = '/configuration';
 
-        var self = this;
-        self.create = create;
+      var self = this;
+      self.create = create;
 
-        function create() {
-            var restPrefix = OtusRestResourceContext.getRestPrefix();
-            var token = OtusRestResourceContext.getSecurityToken();
-            var headers = HeaderBuilderFactory.create(token);
-            var headersPublishTemplate = HeaderBuilderFactory.create(token);
-            headersPublishTemplate.setContentType('application/json');
+      function create() {
+         var restPrefix = OtusRestResourceContext.getRestPrefix();
+         var token = OtusRestResourceContext.getSecurityToken();
+         var headers = HeaderBuilderFactory.create(token);
+         var headersPublishTemplate = HeaderBuilderFactory.create(token);
+         headersPublishTemplate.setContentType('application/json');
 
-            var config = {
-                getSurveys: {
-                    method: 'GET',
-                    url: restPrefix + SUFFIX + '/surveys',
-                    headers: headers.json
-                },
-                updateSurveyTemplateType: {
-                    method: 'PUT',
-                    url: restPrefix + SUFFIX + '/surveys/:acronym/type',
-                    data: {
-                        'newSurveyFormType': '@newSurveyFormType'
-                    },
-                    headers: headers.json,
-                    params: {
-                        'acronym': '@acronym'
-                    }
-                },
-                publishTemplate: {
-                    method: 'POST',
-                    url: restPrefix + SUFFIX + '/publish/template',
-                    headers: headersPublishTemplate.json
-                },
-                //DELETE -> /configuration/surveys/:acronym
-                getVisualIdentity: {
-                    method: 'GET',
-                    url: restPrefix + SUFFIX + '/visual-identity',
-                    headers: headers.json
-                },
-                updateVisualIdentity: {
-                    method: 'POST',
-                    url: restPrefix + SUFFIX + '/visual-identity',
-                    headers: headers.json
-                }
-            };
-            return $resource({}, {}, config);
-        }
-        return self;
+         var config = {
+            getSurveys: {
+               method: 'GET',
+               url: restPrefix + SUFFIX + '/surveys',
+               headers: headers.json
+            },
+            updateSurveyTemplateType: {
+               method: 'PUT',
+               url: restPrefix + SUFFIX + '/surveys/:acronym/type',
+               data: {
+                  'newSurveyFormType': '@newSurveyFormType'
+               },
+               headers: headers.json,
+               params: {
+                  'acronym': '@acronym'
+               }
+            },
+            publishTemplate: {
+               method: 'POST',
+               url: restPrefix + SUFFIX + '/publish/template',
+              //  interceptor: {
+              //     'response': function (response) {console.log(response);}
+              //  },
+              //  data: {
+              //     'template': '@template'
+              //  },
+               headers: headersPublishTemplate.json
+            },
+            deleteSurveyTemplate: {
+               method: 'DELETE',
+               url: restPrefix + SUFFIX + '/surveys/:acronym',
+               headers: headers.json
+            },
+            getVisualIdentity: {
+               method: 'GET',
+               url: restPrefix + SUFFIX + '/visual-identity',
+               headers: headers.json
+            },
+            updateVisualIdentity: {
+               method: 'POST',
+               url: restPrefix + SUFFIX + '/visual-identity',
+               headers: headers.json
+            }
+         };
+         return $resource({}, {}, config);
+      }
+      return self;
 
-    }
+   }
 }());
