c50a49878aa921715825546c344616c385602fa8
Tiago Matana <tiago.matana@gmail.com>
2018-03-26 12:23:15 -0300

    OCJ-12 test for UrlParser service

--- tests/unit/url-parser-spec.js
+++ tests/unit/url-parser-spec.js
@@ -4,31 +4,37 @@ describe('UrlParser test', function() {
   var $resource;
   // angular.mock.module('otus.client');
   beforeEach(function() {
-
     angular.mock.module('otus.client');
   });
 
-  beforeEach(inject(function(_$injector_) {
-    // $provide.provider('urlParser', UrlParserProvider);
-    SERVICE = _$injector_.get('UrlParser');
-  }));
-
+  beforeEach(function(){
+    inject(function(_$injector_) {
+    mockService(_$injector_);
+  });
+  });
+  
   describe('method parser', function() {
     var parserElement = document.createElement('a');
     beforeEach(function() {
       spyOn(SERVICE, 'parser');
-      parserElement = SERVICE.parser("www");
     });
-
+    
     it('should called method parser', function() {
+      parserElement = SERVICE.parser("www");
       SERVICE.parser("www");
       expect(SERVICE.parser).toHaveBeenCalledWith("www");
       expect(SERVICE.parser).not.toBeNull();
       expect(SERVICE.parser).toBeDefined();
-      expect(SERVICE.parser()).toBe(undefined);
-    })
-
-
+      expect(SERVICE.parser(jasmine.any(String))).toBe(undefined);
+      expect(SERVICE.parser).toEqual(jasmine.any(Function));
+    });
+    
+    
   });
+  
+  function mockService(_$injector_) {
+    SERVICE = _$injector_.get("UrlParser");
+    SERVICE.parser(jasmine.any(String));
+  }
 
 });
