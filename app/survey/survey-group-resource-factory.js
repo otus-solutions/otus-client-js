f730d75011668c65ba73bee46bd88d7d26537795
Adonis Garcia <adonis.garcia.adg@gmail.com>
2019-02-27 13:29:52 -0300

    OCJ-15 parcial surveyGroupResource

---
+++ app/survey/survey-group-resource-factory.js
@@ -0,0 +1,54 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('otus.client')
+    .factory('otus.client.SurveyGroupResourceFactory', SurveyGroupResourceFactory);
+
+  SurveyGroupResourceFactory.$inject = [
+    '$resource',
+    'OtusRestResourceContext',
+    'otus.client.HeaderBuilderFactory'
+  ];
+
+  function SurveyGroupResourceFactory($resource, OtusRestResourceContext, HeaderBuilderFactory) {
+    var SUFFIX = '/survey';
+
+    var self = this;
+
+    /* Public methods */
+    self.create = create;
+
+    function create() {
+      var restPrefix = OtusRestResourceContext.getRestPrefix();
+      var token = OtusRestResourceContext.getSecurityToken();
+      var headers = HeaderBuilderFactory.create(token);
+
+      return $resource({}, {}, {
+        addNewGroup: {
+          method: 'POST',
+          url: restPrefix + SUFFIX + '/new-group',
+          headers: headers.json
+        },
+        getListOfSurveyGroups: {
+          method: 'GET',
+          url: restPrefix + SUFFIX + '/group',
+          headers: headers.json
+        },
+        editGroup: {
+          method: 'PUT',
+          // url: restPrefix + SUFFIX + '/?',
+          headers: headers.json
+        },
+        deleteGroup: {
+          method: 'GET',
+          // url: restPrefix + SUFFIX + '/?',
+          headers: headers.json
+        }
+      });
+    }
+
+    return self;
+  }
+
+}());
