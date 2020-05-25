ee197f0de0c34077ae49fb6696e3999fc44c0161
leogut <leogut@gmail.com>
2020-05-25 14:28:21 -0300

    OCJ-65 Ajuste no arquivo de testes unitários.

--- tests/unit/project-communication/project-communication-resource-factory-spec.js
+++ tests/unit/project-communication/project-communication-resource-factory-spec.js
@@ -12,6 +12,17 @@
     var METHOD_POST_VALUE = "POST";
     var METHOD_PUT_VALUE = "PUT";
 
+    var ID_PARAMETER = {'id': 1234567};
+    var LIMIT_PARAMETER = {'limit': 3};
+
+    var SUFFIX_POST_CREATE_ISSUE = SUFFIX + '/issue/create';
+    var SUFFIX_POST_CREATE_MESSAGE = SUFFIX + '/issue/message/1234567';
+    var SUFFIX_PUT_UPDATE_REOPEN = SUFFIX + '/issue/1234567/reopen';
+    var SUFFIX_PUT_UPDATE_CLOSE = SUFFIX + '/issue/1234567/close';
+    var SUFFIX_GET_MESSAGE_BY_ID = SUFFIX + '/issue/1234567/messages';
+    var SUFFIX_GET_MESSAGE_BY_ID_LIMIT = SUFFIX + '/issue/1234567/messages/3';
+    var SUFFIX_GET_LIST_ISSUE = SUFFIX + '/issue/list';
+
     var factory, factoryResult, otusRestResourceContext, headerBuilderFactory;
     var httpBackend;
 
@@ -25,13 +36,13 @@
         spyOn(otusRestResourceContext, 'getSecurityToken');
         spyOn(headerBuilderFactory, 'create').and.callThrough();
         httpBackend = _$injector_.get('$httpBackend');
-        httpBackend.when(METHOD_POST_VALUE, REST_PREFIX + SUFFIX + '/issue/create').respond(200, DATA);
-        httpBackend.when(METHOD_POST_VALUE, REST_PREFIX + SUFFIX + '/issue/message/:id').respond(200, DATA);
-        httpBackend.when(METHOD_PUT_VALUE, REST_PREFIX + SUFFIX + '/issue/:id/reopen').respond(200, DATA);
-        httpBackend.when(METHOD_PUT_VALUE, REST_PREFIX + SUFFIX + '/issue/:id/close').respond(200, DATA);
-        httpBackend.when(METHOD_GET_VALUE, REST_PREFIX + SUFFIX + '/issue/:id/messages').respond(200, DATA);
-        httpBackend.when(METHOD_GET_VALUE, REST_PREFIX + SUFFIX + '/issue/:id/messages/:limit').respond(200, DATA);
-        httpBackend.when(METHOD_GET_VALUE, REST_PREFIX + SUFFIX + '/issue/list').respond(200, DATA);
+        httpBackend.when(METHOD_POST_VALUE, REST_PREFIX + SUFFIX_POST_CREATE_ISSUE).respond(200, DATA);
+        httpBackend.when(METHOD_POST_VALUE, REST_PREFIX + SUFFIX_POST_CREATE_MESSAGE).respond(200, DATA);
+        httpBackend.when(METHOD_PUT_VALUE, REST_PREFIX + SUFFIX_PUT_UPDATE_REOPEN).respond(200, DATA);
+        httpBackend.when(METHOD_PUT_VALUE, REST_PREFIX + SUFFIX_PUT_UPDATE_CLOSE).respond(200, DATA);
+        httpBackend.when(METHOD_GET_VALUE, REST_PREFIX + SUFFIX_GET_MESSAGE_BY_ID).respond(200, DATA);
+        httpBackend.when(METHOD_GET_VALUE, REST_PREFIX + SUFFIX_GET_MESSAGE_BY_ID_LIMIT).respond(200, DATA);
+        httpBackend.when(METHOD_GET_VALUE, REST_PREFIX + SUFFIX_GET_LIST_ISSUE).respond(200, DATA);
       });
     });
 
@@ -71,52 +82,52 @@
           httpBackend.flush();
         });
 
-        it('createIssue check', function () {
-          var add = factoryResult.createIssue();
-          add.$promise.then(function (resultCreate) {
-            expect(resultCreate.data).toEqual(DATA_CONFIRMATION);
+        it('createIssue method check', function () {
+          var post = factoryResult.createIssue();
+          post.$promise.then(function (resultCreateIssue) {
+            expect(resultCreateIssue.data).toEqual(DATA_CONFIRMATION);
           });
         });
 
-        it('createMessage check', function () {
-          var create = factoryResult.createMessage();
-          create.$promise.then(function (resultCreate) {
-            expect(resultCreate.data).toEqual(DATA_CONFIRMATION);
+        it('createMessage method check', function () {
+          var post = factoryResult.createMessage(ID_PARAMETER);
+          post.$promise.then(function (resultCreateMessage) {
+            expect(resultCreateMessage.data).toEqual(DATA_CONFIRMATION);
           });
         });
 
-        it('updateReopen check', function () {
-          var create = factoryResult.updateReopen();
-          create.$promise.then(function (resultCreate) {
-            expect(resultCreate.data).toEqual(DATA_CONFIRMATION);
+        it('updateReopen method check', function () {
+          var put = factoryResult.updateReopen(ID_PARAMETER);
+          put.$promise.then(function (resultUpdateReopen) {
+            expect(resultUpdateReopen.data).toEqual(DATA_CONFIRMATION);
           });
         });
 
-        it('updateClose check', function () {
-          var create = factoryResult.updateClose();
-          create.$promise.then(function (resultCreate) {
-            expect(resultCreate.data).toEqual(DATA_CONFIRMATION);
+        it('updateClose method check', function () {
+          var put = factoryResult.updateClose(ID_PARAMETER);
+          put.$promise.then(function (resultUpdateClose) {
+            expect(resultUpdateClose.data).toEqual(DATA_CONFIRMATION);
           });
         });
 
-        it('getMessageById check', function () {
-          var create = factoryResult.getMessageById();
-          create.$promise.then(function (resultCreate) {
-            expect(resultCreate.data).toEqual(DATA_CONFIRMATION);
+        it('getMessageById method check', function () {
+          var get = factoryResult.getMessageById(ID_PARAMETER);
+          get.$promise.then(function (resultGetMessageById) {
+            expect(resultGetMessageById.data).toEqual(DATA_CONFIRMATION);
           });
         });
 
-        it('getMessageByIdLimit check', function () {
-          var create = factoryResult.getMessageByIdLimit();
-          create.$promise.then(function (resultCreate) {
-            expect(resultCreate.data).toEqual(DATA_CONFIRMATION);
+        it('getMessageByIdLimit method check', function () {
+          var get = factoryResult.getMessageByIdLimit(ID_PARAMETER,LIMIT_PARAMETER);
+          get.$promise.then(function (resultGetMessageByIdLimit) {
+            expect(resultGetMessageByIdLimit.data).toEqual(DATA_CONFIRMATION);
           });
         });
 
-        it('listIssue check', function () {
-          var create = factoryResult.listIssue();
-          create.$promise.then(function (resultCreate) {
-            expect(resultCreate.data).toEqual(DATA_CONFIRMATION);
+        it('listIssue method check', function () {
+          var get = factoryResult.listIssue();
+          get.$promise.then(function (resultListIssue) {
+            expect(resultListIssue.data).toEqual(DATA_CONFIRMATION);
           });
         });
       });
